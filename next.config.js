a2fe31c3dcdc0cf51125de16e67da137cce0f219
iAsted82 <iasted@me.com>
2025-07-11 21:06:10 +0200

    Résolution des problèmes de cache Webpack

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  webpack: (config, { dev }) => {
+    // Disable webpack cache in development if needed
+    // Uncomment the next line if you experience persistent cache issues
+    // if (dev) config.cache = false;
+    return config;
+  },
 };
 
 module.exports = nextConfig;
