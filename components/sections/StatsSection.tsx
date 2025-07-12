2c68b3524f48577db314efe88d100fdd6567b2ab
iAsted82 <iasted@me.com>
2025-07-12 09:02:51 +0200

    Updated StatsSection.tsx

--- components/sections/StatsSection.tsx
+++ components/sections/StatsSection.tsx
@@ -50,6 +50,7 @@ const stats = [
 export function StatsSection() {
   return (
     <section className="py-20 bg-gradient-to-br from-gray-50 to-white relative overflow-hidden">
+      {/* Background Pattern */}
       <div className="absolute inset-0 opacity-5">
         <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23003F7F" fill-opacity="0.1"%3E%3Cpath d="M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4z"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] bg-repeat"></div>
       </div>
